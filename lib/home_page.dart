ba0bdc88f5be1124783c9c477859c34ca6c98f89
Cirrus CI <support@cirruslabs.org>
2020-01-26 22:23:30 +0000

    add basic functionality

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:dio/dio.dart';
 import 'package:image/image.dart' as Images;
+import 'dart:convert';
 import 'dart:io';
 
 class HomePage extends StatefulWidget {
@@ -10,31 +13,61 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  File image;
+  Uint8List inputImage;
+  Uint8List outputImage;
+
+  int mapColor(int color) {
+    final red = Images.getRed(color);
+    switch (red) {
+      case 1:
+        return Images.setRed(color, 255);
+      case 2:
+        return Images.setGreen(color, 255);
+      case 3:
+        return Images.setBlue(color, 255);
+      default:
+        print(red);
+        return 0;
+    }
+  }
 
   Future getImage() async {
     final selectedImage = await ImagePicker.pickImage(source: ImageSource.camera);
+    final resizedImage = await resizeImage(selectedImage);
+    final resizedJpg = Images.encodeJpg(resizedImage);
     setState(() {
-      image = selectedImage;
+      inputImage = resizedJpg;
     });
-    await analyzeImage(selectedImage);
+    final response = await analyzeImage(resizedJpg);
+    final output = base64Decode(response.data);
+    final decodedOutput = Images.decodePng(output);
+    for (int y = 0; y < decodedOutput.height; y++) {
+      for (int x = 0; x < decodedOutput.width; x++) {
+        final color = decodedOutput.getPixel(x, y);
+        final updatedColor = mapColor(color);
+        decodedOutput.setPixel(x, y, updatedColor);
+      }
+    }
+    final colorizedImage = Images.encodeJpg(decodedOutput);
+    setState(() {
+      outputImage = colorizedImage;
+    });
   }
 
-  Future<Response> analyzeImage(File file) async {
+  Future<Images.Image> resizeImage(File file) async {
     final data = await file.readAsBytes();
     final image = Images.decodeImage(data);
-    final resizedImage = Images.copyResize(image, width: 256);
-    final resizedImageData = Images.encodeJpg(resizedImage);
+    return Images.copyResize(image, width: 512);
+  }
+
+  Future<Response> analyzeImage(Uint8List image) async {
     FormData formData = new FormData.fromMap({
-      "file": MultipartFile.fromBytes(resizedImageData),
+      'file': MultipartFile.fromBytes(image),
     });
-    try {
-      final response = await Dio().post("https://tfkpb80bza.execute-api.us-east-1.amazonaws.com/dev/inference", data: formData);
-      print(response);
-      return response;
-    } catch (e) {
-      print(e);
-    }
+    return await Dio().post(
+      'https://tfkpb80bza.execute-api.us-east-1.amazonaws.com/dev/inference',
+      data: formData,
+    );
   }
 
   @override
@@ -43,12 +76,19 @@ class HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Deep Kitten'),
       ),
-      body: Center(
-        child: Card(
-          child: image == null
-            ? Text('No image selected.')
-            : Image.file(image)
-        ),
+      body: ListView(
+        children: [
+          Card(
+            child: inputImage == null
+              ? Text('No image selected.')
+              : Image.memory(inputImage)
+          ),
+          Card(
+            child: outputImage == null
+              ? Text('No image selected.')
+              : Image.memory(outputImage)
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: getImage,
